d88b0502d6fc12a159dd63b9710e04479906122e
Nikolas <nikus@MacBook-Pro-Micha-2.local>
2023-07-31 23:15:11 +0200

    Miki commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'' , loadChildren:() => import('../modules/square/square.module').then(m => m.SquareModule)},
+  {path:'quiz', loadChildren:() => import('../modules/quiz/quiz.module').then(m =>  m.QuizModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
